068039013a18575bcb17f2baca129c07c9b5b593
antoninbeaufort <antonin.beaufort@gmail.com>
2022-06-08 11:02:10 +0200

    🧐

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,35 @@
   <v-app>
     <v-main>
       <v-form ref="form">
-        <v-select
-          v-model="selected"
+        <v-combobox
+          v-model="comboboxSelected"
           variant="underlined"
           :items="items"
           label="Item"
-          :rules="[(v) => !!v || 'Item required']"
+          :rules="[(v) => !!v || 'Requis']"
+        />
+        <v-combobox
+          v-model="comboboxSelectedValues"
+          variant="underlined"
+          :items="items"
+          label="Item"
+          multiple
+          :rules="[(v) => !!v || 'Requis']"
+        />
+        <v-combobox
+          v-model="comboboxComplexSelected"
+          variant="underlined"
+          :items="complexItems"
+          label="Item"
+          :rules="[(v) => !!v || 'Requis']"
+        />
+        <v-combobox
+          v-model="comboboxComplexSelectedValues"
+          variant="underlined"
+          :items="complexItems"
+          label="Item"
+          multiple
+          :rules="[(v) => !!v || 'Requis']"
         />
         <v-btn @click="checkValidState">
           Submit
@@ -20,11 +43,26 @@
 <script setup>
 import { ref } from 'vue'
 const form = ref(null)
-const selected = ref(null)
 const items = ref([
   'Item 1',
   'Item 2',
 ])
+
+const complexItems = ref([
+  {
+    title: 'Item 1',
+    value: 'item-1',
+  },
+  {
+    title: 'Item 2',
+    value: 'item-2',
+  },
+])
+
+const comboboxSelected = ref(null)
+const comboboxSelectedValues = ref(null)
+const comboboxComplexSelected = ref(null)
+const comboboxComplexSelectedValues = ref(null)
 
 const checkValidState = () => {
   form.value.validate()
